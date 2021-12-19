1e96eb88b00d5e9c5aad8b12e6fe559f61904083
SQ <wsq2001@outlook.com>
2021-12-19 14:37:47 +0800

    主程序

--- src/Service/impl/ArticleServiceImpl.java
+++ src/Service/impl/ArticleServiceImpl.java
@@ -9,46 +9,47 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+
 @Service
 public class ArticleServiceImpl implements ArticleService {
     @Autowired
-    ArticleDao articleDao;
+    private ArticleDao articleDao;
     @Autowired
-    Map<String,Object> paramMap;
+    private Map<String, Object> paramMap;
 
     @Override
     public Blog getArticle(String blogId) {
         paramMap.clear();
-        if(blogId==null||blogId.equals("")||blogId.equals("null"))
-            paramMap.put("blogId",1);
+        if (blogId == null || blogId.equals("") || blogId.equals("null"))
+            paramMap.put("blogId", 1);
         else
-            paramMap.put("blogId",blogId);
+            paramMap.put("blogId", blogId);
         return articleDao.getArticle(paramMap);
     }
 
     @Override
     public List<Comment> getComment(String blogId) {
         paramMap.clear();
-        if(blogId==null||blogId.equals("")||blogId.equals("null"))
-            paramMap.put("blogId",1);
+        if (blogId == null || blogId.equals("") || blogId.equals("null"))
+            paramMap.put("blogId", 1);
         else
-            paramMap.put("blogId",blogId);
+            paramMap.put("blogId", blogId);
         return articleDao.getComment(paramMap);
     }
 
     @Override
     public Integer setComment(String commentText, Integer blogId, String commentAuthor) {
         paramMap.clear();
-        paramMap.put("commentText",commentText);
-        paramMap.put("blogId",blogId);
-        paramMap.put("commentAuthor",commentAuthor);
+        paramMap.put("commentText", commentText);
+        paramMap.put("blogId", blogId);
+        paramMap.put("commentAuthor", commentAuthor);
         return articleDao.setComment(paramMap);
     }
 
     @Override
     public Integer delComment(Integer commentId) {
         paramMap.clear();
-        paramMap.put("commentId",commentId);
+        paramMap.put("commentId", commentId);
         return articleDao.delComment(paramMap);
     }
 }
